30f08068fe6d373c468f318802912227895f636b
Juliana Bearzi <julietad299@gmail.com>
2021-08-28 13:52:52 -0300

    initial server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
