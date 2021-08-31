904db51e69095b2f40659c38c5f41989b16f8123
Juliana Bearzi <julietad299@gmail.com>
2021-08-31 08:49:52 -0300

    modifications for building controllers

--- routes/buildings.routes.js
+++ routes/buildings.routes.js
@@ -3,7 +3,13 @@ const router = express.Router();
 const buildingsController = require('../controllers/buildings.controller');
 
 router.get('/buildings', buildingsController.getAllBuildings);
-router.get('/buildings/:id', buildingsController.getBuildingById);
+router.get('/buildings/:param', (req, res) => {
+    if (req.params.param.match(/^[0-9]*$/)) {
+        buildingsController.getBuildingById(req, res);
+    } else {
+        buildingsController.getBuildingByType(req, res);
+    }
+});
 router.delete('/buildings/:id', buildingsController.deleteBuilding);
 
 module.exports = router;
