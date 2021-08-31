5ab4198e8ba50def181ab59fc8ce841390ff030c
carla2807 <cm90_mdp@hotmail.com>
2021-08-31 17:22:48 -0300

    Add Constructions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const ConstructionRoutes = require('./routes/construction-company.routes');
 const app = express();
 const PORT = 3000;
+
+app.use(express.json());
+app.use(ConstructionRoutes);
 
 app.get('/', (req, res) => {
   res.send('hi');
@@ -9,3 +13,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+
