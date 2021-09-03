afa8e44307091f2594e865837d9bd5ae9eb0a32e
Juliana Bearzi <julietad299@gmail.com>
2021-09-03 14:21:20 -0300

    modifications in get methods

--- controllers/boilers.controller.js
+++ controllers/boilers.controller.js
@@ -5,22 +5,22 @@ const getAllBoilers = (req, res) => {
 };
 
 const getBoilerById = (req, res) => {
-    const idFound = boilers.some(boiler => boiler.id === parseInt(req.params.param));
+    const idFound = boilers.some(boiler => boiler.id === parseInt(req.params.id));
 
     if(idFound) {
-        res.json(boilers.filter(boiler => boiler.id === parseInt(req.params.param)));
+        res.json(boilers.filter(boiler => boiler.id === parseInt(req.params.id)));
     } else {
-        res.status(400).json({ msg: `No boiler with the id of ${req.params.param}`});
+        res.status(400).json({ msg: `No boiler with the id of ${req.params.id}`});
     }
 };
 
 const getBoilerByCategory = (req, res) => {
-    const catFound = boilers.some(boiler => boiler.category === req.params.param);
+    const catFound = boilers.some(boiler => boiler.category.toLowerCase() === req.query.category.toLowerCase());
 
     if(catFound) {
-        res.json(boilers.filter(boiler => boiler.category === req.params.param));
+        res.json(boilers.filter(boiler => boiler.category.toLowerCase() === req.query.category.toLowerCase()));
     } else {
-        res.status(400).json({ msg: `No boiler category ${req.params.param}`});
+        res.status(400).json({ msg: `No boiler category ${req.query.category}`});
     }
 };
 
--- routes/boilers.routes.js
+++ routes/boilers.routes.js
@@ -3,14 +3,8 @@ const router = express.Router();
 const boilersController = require('../controllers/boilers.controller');
 
 router.get('/boilers', boilersController.getAllBoilers);
-router.get('/boilers/:param', (req, res) => {
-    if (req.params.param.match(/^[0-9]*$/)) {
-        boilersController.getBoilerById(req, res);
-    } 
-    else {
-        boilersController.getBoilerByCategory(req, res);
-    }
-});
+router.get('/boilers/search', boilersController.getBoilerByCategory);
+router.get('/boilers/:id', boilersController.getBoilerById);
 router.delete('/boilers/:id', boilersController.deleteBoiler);
 
 module.exports = router;
