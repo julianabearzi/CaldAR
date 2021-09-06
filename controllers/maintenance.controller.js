5bc2b8f64268d138dc57354aa85c03ec4f95f499
Juliana Bearzi <julietad299@gmail.com>
2021-09-05 21:53:01 -0300

    put and post methods for maintenance

--- controllers/maintenance.controller.js
+++ controllers/maintenance.controller.js
@@ -1,4 +1,30 @@
-const maintenance = require('../data/maintenance.json');
+const fs = require('fs');
+
+const maintenances = fs.readFileSync('data/maintenance.json');
+let maintenance = JSON.parse(maintenances);
+
+const createMaintenance = (req, res) => {
+
+    const { id, boiler, date, technicians_list, building } = req.body;
+
+    if (!id || !boiler || !date || !technicians_list || !building) {
+        res.status(400).send("Incomplete fields");
+        return;
+    }
+
+    const newMaintenance = {
+        id,
+        boiler,
+        date,
+        technicians_list,
+        building
+    };
+
+    maintenance.push(newMaintenance);
+    fs.writeFileSync('data/maintenance.json', JSON.stringify(maintenance, null, 2));
+    res.json(newMaintenance);
+};
+
 
 const getAllMaintenance = (req, res) => {
     res.json(maintenance);
@@ -26,20 +52,54 @@ const getMaintenanceByBoiler = (req, res) => {
     }
 };
 
-const deleteMaintenance = (req, res) => {
-    const idFound = maintenance.some(m => m.id === parseInt(req.params.id));
+const updateMaintenance = (req, res) => {
 
-    if (idFound) {
-        res.json({ msg: 'Maintenance deleted', maintenance: maintenance.filter(m => m.id !== parseInt(req.params.id)) });
+    const id = parseInt(req.params.id);
+    const { boiler = '', date = '', technicians_list = '', building = '' } = req.body;
+    const maintenanceItem = maintenance.find(maintenanceItem => maintenanceItem.id === parseInt(req.params.id));
+    if (maintenanceItem) {
+        const index = maintenance.indexOf(maintenanceItem);
+        const updatedMaintenance = {
+            id,
+            boiler,
+            date,
+            technicians_list,
+            building
+        };
+
+        if (!boiler || !date || !technicians_list || !building) {
+            res.status(400).send("Incomplete fields");
+            return;
+        }
+
+        maintenance[index] = updatedMaintenance;
+
+        fs.writeFileSync('data/maintenance.json', JSON.stringify(maintenance, null, 2));
+        res.json({ msg: 'Maintenance updated', updatedMaintenance });
     } else {
         res.status(400).json({ msg: `No maintenance with the id of ${req.params.id}` });
     }
 };
 
+const deleteMaintenance = (req, res) => {
+    const idFound = maintenance.some(m => m.id === parseInt(req.params.id));
+
+    if (idFound) {
+        maintenance = maintenance.filter(m => m.id !== parseInt(req.params.id));
+
+        fs.writeFileSync('data/maintenance.json', JSON.stringify(maintenance, null, 2));
+        res.json({ msg: 'Maintenance deleted', maintenance });
+
+    } else {
+        res.status(400).json({ msg: `No maintenance with the id of ${req.params.id}` });
+    }
+};
 
 module.exports = {
+    createMaintenance,
     getAllMaintenance,
     getMaintenanceById,
     getMaintenanceByBoiler,
-    deleteMaintenance
+    deleteMaintenance,
+    updateMaintenance
 };
--- routes/maintenance.routes.js
+++ routes/maintenance.routes.js
@@ -5,6 +5,8 @@ const maintenanceController = require('../controllers/maintenance.controller');
 router.get('/maintenance', maintenanceController.getAllMaintenance);
 router.get('/maintenance/search', maintenanceController.getMaintenanceByBoiler);
 router.get('/maintenance/:id', maintenanceController.getMaintenanceById);
+router.post('/maintenance', maintenanceController.createMaintenance);
 router.delete('/maintenance/:id', maintenanceController.deleteMaintenance);
+router.put('/maintenance/:id', maintenanceController.updateMaintenance);
 
 module.exports = router;
