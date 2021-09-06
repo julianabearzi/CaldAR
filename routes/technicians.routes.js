3e46e8b39e448a39ce5c4e6e7a9c03e9c7f1701b
carla2807 <cm90_mdp@hotmail.com>
2021-09-06 20:21:20 -0300

    Add POST and PUT methods

--- routes/technicians.routes.js
+++ routes/technicians.routes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const techniciansController = require('../controllers/technicians.controller');
 
 router.get('/technicians', techniciansController.getAllTechnicians);
-
 router.get('/technicians/search', techniciansController.getTechnicianByFirstName);
-
 router.get('/technicians/:id', techniciansController.getTechnicianById);
-
+router.post('/technicians', techniciansController.createTechnician);
 router.delete('/technicians/:id', techniciansController.deleteTechnician);
+router.put('/technicians/:id',techniciansController.updateTechnician);
 
 module.exports = router;
