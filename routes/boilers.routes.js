46759549eeb59270f806576fc1fe3f79dccd8fd5
Juliana Bearzi <julietad299@gmail.com>
2021-09-06 11:16:07 -0300

    add post and put methods for boilers

--- routes/boilers.routes.js
+++ routes/boilers.routes.js
@@ -5,6 +5,8 @@ const boilersController = require('../controllers/boilers.controller');
 router.get('/boilers', boilersController.getAllBoilers);
 router.get('/boilers/search', boilersController.getBoilerByCategory);
 router.get('/boilers/:id', boilersController.getBoilerById);
+router.post('/boilers', boilersController.createBoiler);
 router.delete('/boilers/:id', boilersController.deleteBoiler);
+router.put('/boilers/:id', boilersController.updateBoiler);
 
 module.exports = router;
