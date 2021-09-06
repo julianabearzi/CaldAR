6efe6660939a448778275449f1fe545c82550ac6
Juliana Bearzi <julietad299@gmail.com>
2021-09-06 10:38:25 -0300

    add post and put methods for buildings

--- routes/buildings.routes.js
+++ routes/buildings.routes.js
@@ -5,6 +5,8 @@ const buildingsController = require('../controllers/buildings.controller');
 router.get('/buildings', buildingsController.getAllBuildings);
 router.get('/buildings/search', buildingsController.getBuildingByType);
 router.get('/buildings/:id', buildingsController.getBuildingById);
+router.post('/buildings', buildingsController.createBuilding);
 router.delete('/buildings/:id', buildingsController.deleteBuilding);
+router.put('/buildings/:id', buildingsController.updateBuilding);
 
 module.exports = router;
