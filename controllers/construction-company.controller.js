54105012ebb56eb4889270c530610fa174fa4d71
carla2807 <cm90_mdp@hotmail.com>
2021-09-07 19:13:12 -0300

    Add POST and PUT methods

--- controllers/construction-company.controller.js
+++ controllers/construction-company.controller.js
@@ -1,39 +1,103 @@
-const constructions = require('../data/construction-company.json');
+const fs = require('fs');
+
+const constructions = fs.readFileSync('data/construction-company.json');
+
+let construction = JSON.parse(constructions);
+
+const createConstruction = (req, res) => {
+    const {
+        id,
+        first_name
+        
+       
+    } = req.body;
+
+    if (!id || !first_name ) {
+        res.status(400).send("Incomplete fields");
+        return;
+    }
+
+    const newConstruction = {
+        id,
+        first_name,      
+    };
+
+
+    construction.push(newConstruction);
+    fs.writeFileSync('data/construction-company.json', JSON.stringify(construction, null, 2));
+    res.json(newConstruction);
+
+};
 
 const getAllConstructions = (req,res) =>{
-    res.json(constructions);
+    res.json(construction);
 }
 
+
 const getConstructionsById = (req,res) =>{
-    const found = constructions.some(construction => construction.id === parseInt(req.params.param));
+    const found = construction.some(c => c.id === parseInt(req.params.id));
     if (found) {
-        res.json(constructions.filter(construction => construction.id === parseInt(req.params.param)));
+        res.json(constructions.filter(c => c.id === parseInt(req.params.id)));
     }
     else{
-        res.status(404).json({ msg: `No construction with the id of ${req.params.param}`});
+        res.status(400).json({ msg: `No construction with the id of ${req.params.id}`});
     }
 }
 
 
 const getConstructionsByFirstName= (req,res) =>{
-    const found = constructions.some(construction =>construction.first_name === req.params.param);
+    const found = construction.some(c =>c.first_name.toLowerCase() === (req.query.first_name.toLowerCase()));
     if (found) {
-        res.json(constructions.filter(construction => construction.first_name === req.params.param));
+        res.json(constructions.filter(c => c.first_name.toLowerCase() === (req.query.first_name.toLowerCase())));
     }
     else{
-        res.status(404).json({ msg: `No construction with the name ${req.params.param}`});
+        res.status(400).json({ msg: `No construction with the name ${req.query.first_name}`});
     }
 }
 
 
-const deleteConstruction = (req,res) =>{
-    const found = constructions.some(construction => construction.id === parseInt(req.params.id));
-    if (found) {
-        res.json({ msg: 'construction deleted', constructions: constructions.filter(construction => construction.id !== parseInt(req.params.id))});
-    }
+const updateConstruction = (req, res) => {
+    const id = parseInt(req.params.id);
+    const {
+        first_name = '',
+    } = req.body;
 
-    else{
-        res.status(400).json({ msg: `No construction with the id of ${req.params.id}`});
+    const constructionItem = construction.find(constructionItem => constructionItem.id === parseInt(req.params.id));
+    if (constructionItem) {
+        const index = construction.indexOf(constructionItem);
+        const updatedConstruction = {
+            id,
+            first_name
+             
+        };
+
+        if (!first_name) {
+            res.status(400).send("Incomplete fields");
+            return;
+        }
+
+        construction[index] = updatedConstruction;
+
+        fs.writeFileSync('data/construction-company.json', JSON.stringify(construction, null, 2));
+        res.json({ msg: 'Construction updated', updatedConstruction });
+    } else {
+        res.status(400).json({ msg: `No construction with the id of ${req.params.id}` });
+    }
+};
+
+
+
+
+const deleteConstruction = (req,res) =>{
+    const found = construction.some(c => c.id === parseInt(req.params.id));
+    
+    if (found) {
+        construction = construction.filter(c => c.id !== parseInt(req.params.id));
+
+        fs.writeFileSync('data/construction-company.json', JSON.stringify(construction, null, 2));
+        res.json({ msg: 'Construction deleted', construction });
+    } else {
+        res.status(400).json({ msg: `No construction with the id of ${req.params.id}` });
     }
 }
 
@@ -41,5 +105,7 @@ module.exports ={
     getAllConstructions,
     getConstructionsById,
     getConstructionsByFirstName,
+    createConstruction,
+    updateConstruction,
     deleteConstruction
 }
--- routes/construction-company.routes.js
+++ routes/construction-company.routes.js
@@ -3,15 +3,10 @@ const router = express.Router();
 const constructionsController = require('../controllers/construction-company.controller');
 
 router.get('/constructions', constructionsController.getAllConstructions);
-router.get('/constructions/:param',(req,res) =>{
-    if (req.params.param.match(/^[0-9]*$/)) {
-        constructionsController.getConstructionsById(req,res);
-    }
-    else{
-        constructionsController.getConstructionsByFirstName(req,res);
-    }
-
-});
-
+router.get('/constructions/search',constructionsController.getConstructionsByFirstName);
+router.get('/constructions/:id',constructionsController.getConstructionsById);
+router.post('/constructions',constructionsController.createConstruction);
 router.delete('/constructions/:id', constructionsController.deleteConstruction);
+router.put('/constructions/:id',constructionsController.updateConstruction);
+
 module.exports = router;
