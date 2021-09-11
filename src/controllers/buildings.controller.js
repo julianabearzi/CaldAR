1d3266d6826083c105e851ca04d1a725516e82a0
Juliana Bearzi <julietad299@gmail.com>
2021-09-10 21:49:28 -0300

    add buildings data and update methods

---
+++ src/controllers/buildings.controller.js
@@ -0,0 +1,144 @@
+const BuildingSchema = require('../model/Buildings')
+
+const createBuilding = async (req, res) => {
+    try {
+        const building = new BuildingSchema(req.body);
+        const newBuilding = await building.save();
+
+        return res.status(201).json({
+            data: newBuilding,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+
+const getAllBuildings = async (req, res) => {
+    try {
+        const response = await BuildingSchema.find();
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+const getBuildingById = async (req, res) => {
+    try {
+        const response = await BuildingSchema.findOne({ _id: req.params.id});
+
+        if(!response || response.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No building with the id of ${req.params.id}`
+            });
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+}
+
+const getBuildingByName = async (req, res) => {
+    try {
+        const response = await BuildingSchema.findOne({ name: req.query.name});
+
+        if(!response) {
+            return res.status(404).json({
+                error: true,
+                msg: `No building with the name ${req.query.name}`
+            });
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+}
+
+const updateBuilding = async (req, res) => {
+    try {
+        const buildingUpdated = await BuildingSchema.findOneAndUpdate({ _id: req.params.id}, req.body, { new: true });
+
+        if(!buildingUpdated || buildingUpdated.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No building with the id ${req.params.id}`
+            });
+        }
+
+        return res.status(201).json({
+            msg: 'Building updated',
+            data: buildingUpdated,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+const deleteBuilding = async (req, res) => {
+    try {
+        const buildingFound = await BuildingSchema.findOneAndRemove({ _id: req.params.id});
+
+        if(!buildingFound || buildingFound.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No building with the id ${req.params.id}`
+            });
+        }
+
+        return res.status(202).json({
+            msg: 'Building deleted',
+            data: buildingFound,
+            error: false
+        });
+        
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+module.exports = {
+    createBuilding,
+    getAllBuildings,
+    getBuildingById,
+    getBuildingByName,
+    updateBuilding,
+    deleteBuilding
+};
