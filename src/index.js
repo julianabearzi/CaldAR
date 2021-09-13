a34c41c86df2022ba9e1baec993fc0f04c522375
carla2807 <cm90_mdp@hotmail.com>
2021-09-12 21:56:29 -0300

    add constructions data and update methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ConstructionRoutes = require('./routes/construction-company.routes');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(ConstructionRoutes);
+
+mongoose.connect('mongodb+srv://user:test123@cluster0.taz6e.mongodb.net/CaldAR?retryWrites=true&w=majority')
+.then((result) =>{
+console.log('Database connected')
+})
+.catch((error) =>{
+  console.log(`Database not connected, error:${error}`)
+})
+
 
 app.get('/', (req, res) => {
   res.send('hi');
