b9fbabccb6a1e59197469f0291a3e39041f57599
Juliana Bearzi <julietad299@gmail.com>
2021-09-15 13:18:32 -0300

    add boilers data

---
+++ src/model/Boilers.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const boilerSchema = mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true
+    },
+    categoryId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'boilers-categories',
+      required: true,
+    },
+    boilerSituation: {
+      type: String,
+      enum: ['installed', 'reserved', 'available'],
+      required: true
+    },
+    building: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'buildings'
+    }
+  }, { timestamp: true }
+);
+
+module.exports = mongoose.model('Boilers', boilerSchema);
