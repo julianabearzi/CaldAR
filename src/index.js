039ae0a8ec0b91382c031ee9ea2e00edf51662de
carla2807 <cm90_mdp@hotmail.com>
2021-09-15 17:30:34 -0300

    file organization

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const techniciansRoutes = require('./routes/technicians.routes');
-const ConstructionRoutes = require('./routes/construction-company.routes');
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
 app.use(techniciansRoutes);
-app.use(ConstructionRoutes);
+
+mongoose.connect('mongodb+srv://user:test123@cluster0.taz6e.mongodb.net/CaldAR?retryWrites=true&w=majority')
+.then((result) =>{
+console.log('Database connected')
+})
+.catch((error) =>{
+  console.log(`Database not connected, error:${error}`)
+})
 
 app.get('/', (req, res) => {
   res.send('hi');
