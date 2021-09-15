326e07f38a13f94c1860cd82e11ff2c7fcea0e1d
carla2807 <cm90_mdp@hotmail.com>
2021-09-15 17:29:15 -0300

    Add technicians data and methods

---
+++ src/controllers/technicians.controller.js
@@ -0,0 +1,145 @@
+const TechnicianSchema = require('../model/Technicians');
+
+const createTechnician = async (req, res) => {
+    try {
+        const technician = new TechnicianSchema(req.body);
+        const newTechnician = await technician.save();
+
+        return res.status(201).json({
+            data: newTechnician,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+const getAllTechnicians = async (req, res) => {
+    try {
+        const response = await TechnicianSchema.find();
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+const getTechnicianById = async (req, res) => {
+    try {
+        const response = await TechnicianSchema.findOne({ _id: req.params.id });
+
+        if (!response || response.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No technician with the id of ${req.params.id}`
+            });
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+}
+
+
+const getTechnicianByFirstName = async (req, res) => {
+    try {
+        const response = await TechnicianSchema.findOne({ name: req.query.name });
+
+        if (!response) {
+            return res.status(404).json({
+                error: true,
+                msg: `No technician with the name ${req.query.name}`
+            });
+        }
+
+        return res.status(200).json({
+            data: response,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+}
+
+
+const updateTechnician = async (req, res) => {
+    try {
+        const technicianUpdated = await TechnicianSchema.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true });
+
+        if (!technicianUpdated || technicianUpdated.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No technician with the id ${req.params.id}`
+            });
+        }
+
+        return res.status(201).json({
+            msg: 'Technician updated',
+            data: technicianUpdated,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+const deleteTechnician = async (req, res) => {
+    try {
+        const technicianFound = await TechnicianSchema.findOneAndRemove({ _id: req.params.id });
+
+        if (!technicianFound || technicianFound.length === 0) {
+            return res.status(404).json({
+                error: true,
+                msg: `No technician with the id ${req.params.id}`
+            });
+        }
+
+        return res.status(202).json({
+            msg: 'Technician deleted',
+            data: technicianFound,
+            error: false
+        });
+
+    } catch (error) {
+        return res.status(400).json({
+            error: true,
+            msg: error
+        });
+    }
+};
+
+module.exports = {
+    getAllTechnicians,
+    getTechnicianById,
+    getTechnicianByFirstName,
+    createTechnician,
+    updateTechnician,
+    deleteTechnician
+}
