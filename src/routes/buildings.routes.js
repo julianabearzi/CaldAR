7330482024347131c825fa199f1f48afdedd60f5
Juliana Bearzi <julietad299@gmail.com>
2021-09-20 16:40:24 -0300

    add buildings validations

--- src/routes/buildings.routes.js
+++ src/routes/buildings.routes.js
@@ -2,12 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 const buildingsController = require('../controllers/buildings.controller');
+const {
+  ValidateCreate,
+  ValidateBuildingId,
+} = require('../validators/buildings');
 
 router.get('/buildings', buildingsController.getAllBuildings);
 router.get('/buildings/search', buildingsController.getBuildingByName);
-router.get('/buildings/:id', buildingsController.getBuildingById);
-router.post('/buildings', buildingsController.createBuilding);
-router.delete('/buildings/:id', buildingsController.deleteBuilding);
-router.put('/buildings/:id', buildingsController.updateBuilding);
+router.get(
+  '/buildings/:id',
+  ValidateBuildingId,
+  buildingsController.getBuildingById
+);
+router.post('/buildings', ValidateCreate, buildingsController.createBuilding);
+router.delete(
+  '/buildings/:id',
+  ValidateBuildingId,
+  buildingsController.deleteBuilding
+);
+router.put(
+  '/buildings/:id',
+  ValidateBuildingId,
+  ValidateCreate,
+  buildingsController.updateBuilding
+);
 
 module.exports = router;
