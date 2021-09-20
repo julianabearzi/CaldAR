eb9360f37666aa303a63e6af8d4c50bec0ca73c9
carla2807 <cm90_mdp@hotmail.com>
2021-09-19 22:04:20 -0300

    Validations construction-company

--- src/routes/construction-company.routes.js
+++ src/routes/construction-company.routes.js
@@ -2,15 +2,36 @@ const express = require('express');
 
 const router = express.Router();
 const constructionsController = require('../controllers/construction-company.controller');
+const {
+  ValidateCreate,
+  ValidateConstructionCompanyId,
+} = require('../validators/construction-company');
 
 router.get('/constructions', constructionsController.getAllConstructions);
 router.get(
   '/constructions/search',
   constructionsController.getConstructionByFirstName
 );
-router.get('/constructions/:id', constructionsController.getConstructionById);
-router.post('/constructions', constructionsController.createConstruction);
-router.delete('/constructions/:id', constructionsController.deleteConstruction);
-router.put('/constructions/:id', constructionsController.updateConstruction);
+router.get(
+  '/constructions/:id',
+  ValidateConstructionCompanyId,
+  constructionsController.getConstructionById
+);
+router.post(
+  '/constructions',
+  ValidateCreate,
+  constructionsController.createConstruction
+);
+router.delete(
+  '/constructions/:id',
+  ValidateConstructionCompanyId,
+  constructionsController.deleteConstruction
+);
+router.put(
+  '/constructions/:id',
+  ValidateConstructionCompanyId,
+  ValidateCreate,
+  constructionsController.updateConstruction
+);
 
 module.exports = router;
