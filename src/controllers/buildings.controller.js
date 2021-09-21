068aa28b626ef77bae1c58bb0d93e29c98f061fa
Juliana Bearzi <julietad299@gmail.com>
2021-09-20 22:13:15 -0300

    validations improvements

--- src/controllers/buildings.controller.js
+++ src/controllers/buildings.controller.js
@@ -1,8 +1,14 @@
-const BuildingSchema = require('../model/Buildings');
+const models = require('../model');
 
 const createBuilding = async (req, res) => {
   try {
-    const building = new BuildingSchema(req.body);
+    const type = await models.Constructions.findById(req.body.type);
+    if (!type) {
+      return res.status(400).json({
+        msg: 'The type assigned to building was not found',
+      });
+    }
+    const building = new models.Buildings(req.body);
     const newBuilding = await building.save();
 
     return res.status(201).json({
@@ -19,7 +25,7 @@ const createBuilding = async (req, res) => {
 
 const getAllBuildings = async (req, res) => {
   try {
-    const response = await BuildingSchema.find();
+    const response = await models.Buildings.find();
 
     return res.status(200).json({
       data: response,
@@ -35,7 +41,7 @@ const getAllBuildings = async (req, res) => {
 
 const getBuildingById = async (req, res) => {
   try {
-    const response = await BuildingSchema.findOne({ _id: req.params.id });
+    const response = await models.Buildings.findOne({ _id: req.params.id });
 
     if (!response || response.length === 0) {
       return res.status(404).json({
@@ -58,7 +64,7 @@ const getBuildingById = async (req, res) => {
 
 const getBuildingByName = async (req, res) => {
   try {
-    const response = await BuildingSchema.findOne({ name: req.query.name });
+    const response = await models.Buildings.findOne({ name: req.query.name });
 
     if (!response) {
       return res.status(404).json({
@@ -81,7 +87,7 @@ const getBuildingByName = async (req, res) => {
 
 const updateBuilding = async (req, res) => {
   try {
-    const buildingUpdated = await BuildingSchema.findOneAndUpdate(
+    const buildingUpdated = await models.Buildings.findOneAndUpdate(
       { _id: req.params.id },
       req.body,
       { new: true }
@@ -109,7 +115,7 @@ const updateBuilding = async (req, res) => {
 
 const deleteBuilding = async (req, res) => {
   try {
-    const buildingFound = await BuildingSchema.findOneAndRemove({
+    const buildingFound = await models.Buildings.findOneAndRemove({
       _id: req.params.id,
     });
 
