7d1c1d105f9a2b5bf7153c1fc1781023b696c417
carla2807 <cm90_mdp@hotmail.com>
2021-09-22 14:12:32 -0300

    fix delete construction-company validation

---
+++ src/model/index.js
@@ -0,0 +1,11 @@
+const BoilersCategories = require('./Boilers-categories');
+const Technicians = require('./Technicians');
+const Buildings = require('./Buildings');
+const Constructions = require('./Construction-company');
+
+module.exports = {
+  BoilersCategories,
+  Technicians,
+  Buildings,
+  Constructions,
+};
